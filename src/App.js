4f0ffed30745446bfa69232a795e8cc0c4694e39
Ari Junior <ari_jr12@yahoo.com.br>
2020-06-24 00:31:42 -0300

    Implementação da listagem de itens da api, adicionar na api e remover da api, tudo sendo mostrado em live reload na tela

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setItems(response.data);
+    });
+    
+  },[]);
+
+  //const [repository, setRepository] = useState([]);
+
+  
+
   async function handleAddRepository() {
-    // TODO
+    api.post('/repositories', {
+      url: "https://github.com/Rocketseat/umbriel",
+      title: `ReactJs ${Date.now()}`,
+      techs: ["Node", "Express", "TypeScript"]
+    }).then(response => {
+      setItems([...items,response.data]);
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`).then(response => {
+      const filteredItems = items.filter(item => item.id !== id);
+      setItems(filteredItems);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      {items.map(item => (
+          <li>
+          {item.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(item.id)}>
             Remover
           </button>
         </li>
+      ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
